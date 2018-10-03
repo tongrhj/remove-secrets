26af7e1aa60ed422850cfd4abb797130abda04c8
Jared Tong <tongrhj@users.noreply.github.com>
2018-10-03 17:36:35 +0800

    Remove rotated secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://jaredtong:stack2018@ds121593.mlab.com:21593/stack2018-remove-secrets
+# export MONGODB_CREDENTIALS=mongodb://jaredtong:stack2018@ds121593.mlab.com:21593/stack2018-remove-secrets
 
 # finally, run the app in dev mode...
 npm run dev
